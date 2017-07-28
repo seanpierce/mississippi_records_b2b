59256a1f961171c27c2315855faf2af7699929fe
seanpierce <contact@seanpierce.us>
2017-07-28 10:46:07 -0700

    impliment audio uploading w/ paperclip

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,9 +1,10 @@
 class Album < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
-  has_attached_file :audio
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :audio, content_type: /\AAudio file/
+
+  has_attached_file :audio
+  validates_attachment_content_type :audio, :content_type => ['audio/mpeg']
 
   has_many :order_items, dependent: :destroy
 
