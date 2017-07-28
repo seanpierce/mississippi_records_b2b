d00beb090901103c66738ee49265f4ed61d60fe1
seanpierce <contact@seanpierce.us>
2017-07-28 16:06:39 -0700

    add page dropdown to rails admin #posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,20 @@
 class Post < ApplicationRecord
   validates_presence_of :page, :content
+
   validates :page,
     :inclusion  => { :in => [ 'how_to', 'about', 'links', 'new_arrivals' ],
     :message    => "%{value} is not a valid page" }
+
+    rails_admin do
+      create do
+        field :page, :enum do
+          enum do
+            [['how_to'], ['about'], ['links'], ['new_arrivals']]
+          end
+        end
+        field :content, :text
+      end
+    end
 
   scope :page_posts, -> (page) do
     where("page like ?", "#{page}")
